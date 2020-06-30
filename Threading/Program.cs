7350875b9e2a1a66cd8d19f5b73daf32d35c39b6
Stefan Gherman <scgheman@gmail.com>
2020-06-30 03:03:50 +0300

    Add project files.

---
+++ Threading/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+
+        private static void Counting()
+        {
+            for(int i = 1; i< 11; i++)
+            {
+                Console.WriteLine($"Current count {i}\n Current Thread: {Thread.CurrentThread.ManagedThreadId}");
+            }
+        }
+    }
+}
