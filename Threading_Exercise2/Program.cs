47a1ae6b765f618d8e99b234b246259e743ca4f5
Stefan Gherman <scgheman@gmail.com>
2020-06-30 05:07:51 +0300

    Add solution for mutex exercise

---
+++ Threading_Exercise2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading_Exercise2
+{
+    class Program
+    {
+        private const string mutexName= "RUNMEONLYONCE";
+        static void Main(string[] args)
+        {
+            Mutex mutex;
+            while(1 > 0)
+            {
+                try
+                {
+                    mutex = Mutex.OpenExisting(mutexName);
+                    mutex.Close();
+                    //System.Environment.Exit(1);
+                }
+                catch (WaitHandleCannotBeOpenedException)
+                {
+                    mutex = new Mutex(true, mutexName);
+                   
+                }
+            }
+            
+
+
+        }
+    }
+}
