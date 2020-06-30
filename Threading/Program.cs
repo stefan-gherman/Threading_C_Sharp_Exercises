beda94854b65930eba6fc340f6f300bd93755b8d
Stefan Gherman <scgheman@gmail.com>
2020-06-30 04:25:11 +0300

    Add simple thread loop

--- Threading/Program.cs
+++ Threading/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -11,14 +12,37 @@ namespace Threading
     {
         public static void Main(string[] args)
         {
+            var threadingDelegate = new ThreadStart(() => Counting(2000));
+            var threadingDelegate2 = new ThreadStart(() => Counting(4000));
+            Thread thread1 = new Thread(threadingDelegate);
+            Thread thread2 = new Thread(threadingDelegate);
+            thread1.Name = "First Thread";
+            thread2.Name = "Second Thread";
+            var stopWatch = Stopwatch.StartNew();
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+            Console.WriteLine($"Time elapsed {stopWatch.ElapsedMilliseconds}");
+
+            //stopWatch = Stopwatch.StartNew();
+            //thread1 = new Thread(threadingDelegate);
+            //thread2 = new Thread(threadingDelegate2);
+            //thread1.Name = "First Thread";
+            //thread2.Name = "Second Thread";
+            //thread1.Start();
+            //thread2.Start();
+            //Console.WriteLine($"Time elapsed {stopWatch.ElapsedMilliseconds}");
+            //Counting();
         }
 
 
-        private static void Counting()
+        private static void Counting(int milisecondsAmount)
         {
             for(int i = 1; i< 11; i++)
             {
-                Console.WriteLine($"Current count {i}\n Current Thread: {Thread.CurrentThread.ManagedThreadId}");
+                Console.WriteLine($"Current count {i}\nCurrent Thread: {Thread.CurrentThread.Name}, Status: {Thread.CurrentThread.ThreadState}");
+                Thread.Sleep(milisecondsAmount);
             }
         }
     }
